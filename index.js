ff0a7c53de882df39f5daeaee72741a957f9eba4
Yago Azevedo <yago.dev@gmail.com>
2021-11-14 13:50:14 -0300

    feat: add aluno

--- index.js
+++ index.js
@@ -16,26 +16,23 @@ const getAlunos = (request, response) => {
   });
 };
 
-const addBook = (request, response) => {
-  const { author, title } = request.body;
 
-  pool.query(
-    "INSERT INTO books (author, title) VALUES ($1, $2)",
-    [author, title],
-    (error) => {
-      if (error) {
-        throw error;
-      }
-      response.status(201).json({ status: "success", message: "Book added." });
+const addAluno = (request, response) => {
+  const { nome, cidade, idade } = request.body
+
+  pool.query('INSERT INTO alunos (nome, cidade, idade) VALUES ($1, $2, $3)', [nome, cidade, idade], error => {
+    if (error) {
+      throw error
     }
-  );
-};
+    response.status(201).json({ status: 'success', message: 'Aluno adicionado.' })
+  })
+}
 
 const health = (request, response) => {
   response.status(200).json("Serviço funcionando!");
 };
 
-app.route("/alunos").get(getAlunos).post(addBook)
+app.route("/alunos").get(getAlunos).post(addAluno)
 app.route("/health").get(health);
 
 // Start server
